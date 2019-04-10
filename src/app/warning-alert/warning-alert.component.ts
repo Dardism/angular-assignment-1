4c07a372c5c65124aa2c4d5c8f43b41a3db5db69
Michael Dardis <dardismm@gmail.com>
2019-04-10 14:02:22 -0400

    Assignment 1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: '<h2>This is a warning!</h2>',
+  styles: [` h2 { color: darkred; } `]
+})
+export class WarningAlertComponent {
+
+}
